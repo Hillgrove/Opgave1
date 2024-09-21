9c051dfcb32c59a11fd4f10d22f932c1c363390f
Jesper Højlund <github@hillgrove.dk>
2024-09-21 18:51:22 +0200

    added trophy class

---
+++ Opgave1/Trophy.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Opgave1
+{
+    public class Trophy
+    {
+        private string? _competition;
+        public int Id { get; set; }
+        public string? Competition
+        {
+            get { return _competition; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Competition must be at least 2 characters long");
+                }
+            }
+        }
+    }
+}
